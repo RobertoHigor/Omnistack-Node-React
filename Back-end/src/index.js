f72c5a91abe6b9f80fe9b74af4893774a3106ffb
robertohigor <roberto.higor@outlook.com>
2020-03-25 22:28:00 -0300

    Adicionado cors e removido restrict

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,6 +1,6 @@
 //Importando e instanciando as funcionalidades do express
 const express = require('express');
-const  cors = require('cors');
+const cors = require('cors');
 
 const routes = require('./routes');
 const app = express(); // Desacoplando o express na variável router
