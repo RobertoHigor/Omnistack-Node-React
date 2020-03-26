ffdae8d0a0980d4d7c48311cd28f575d9b207ebb
robertohigor <roberto.higor@outlook.com>
2020-03-25 22:26:43 -0300

    Criado a aplicação front-end do app

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,10 +1,12 @@
 //Importando e instanciando as funcionalidades do express
 const express = require('express');
+const  cors = require('cors');
+
 const routes = require('./routes');
 const app = express(); // Desacoplando o express na variável router
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(3333);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // useState serve para conseguir manipular estados
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+  //No React se utiliza estado para manter uma informação no componente.
+  //É utilizado o método useState(indice); que retorna as seguintes informações
+  //Array [valor, funcaoDeAtualizacao]
+  //Basta então utilizar a funcaoDeAtualização para alterar um valor, funcaoDeAtualizacao(novoValor);
+  // const [counter, setCounter] = useState(0);  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
